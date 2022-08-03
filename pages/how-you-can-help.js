714e33342da29bc551a2f3ca076de188dea68f0e
Tuatoe Daynuah <tuaote.daynuah@gmail.com>
2022-08-02 22:21:12 -0500

    update font

--- pages/how-you-can-help.js
+++ pages/how-you-can-help.js
@@ -14,7 +14,7 @@ const WomenMinistries = () => {
                 <h2 className='page-title'>You Can Make A Difference</h2>
                 <p>There are many ways you could get involved. Below are some ways you could help make a difference in the lives of the underprivileged and disadvantaged children in Liberia: Follow our progress on our website, Facebook, Instagram and Twitter. Help us spread the word.</p>
                 <h3 className='page-sub-title'>Make BKS&#8482; Foundation, Inc. the Charity of Your Choice</h3>
-                <p>Talk to your School/College/Church/University/Company to select BKS&#8482; Foundation, Inc. as their charity of choice for donations. To promote our partnership, we will provide you with our organization&rsquo;s logo and merchandise.</p>
+                <p>Talk to your School, Collge, Church, Univeristy and Company to select BKS&#8482; Foundation, Inc. as their charity of choice for donations. To promote our partnership, we will provide you with our organization&rsquo;s logo and merchandise.</p>
             </div>
         </>
     )
