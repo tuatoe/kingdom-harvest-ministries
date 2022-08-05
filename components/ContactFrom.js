994f116e85124f652f1cec8b791fed6b0047f9c4
Tuatoe Daynuah <tuaote.daynuah@gmail.com>
2022-08-05 13:44:58 -0500

    form

--- components/ContactFrom.js
+++ components/ContactFrom.js
@@ -4,7 +4,7 @@ function ContactFrom({ content }) {
     return (
         <>
             <p>{content}</p>
-            {/* <form action='POST' data-netlify='true' name='bks-contact'>
+            <form action='POST' data-netlify='true' name='bks-contact'>
                 <div className='fields'>
                     <div className='field'>
                         <input type='text' name='name' id='name' placeholder='Your name' required />
@@ -17,31 +17,7 @@ function ContactFrom({ content }) {
                     </div>
                     <div netlify-recaptcha></div>
                 </div>
-                <ul className='actions'>
-                    <li>
-                        <input type='submit' value='Send message' className='btn btn-primary' />
-                    </li>
-                </ul>
-            </form> */}
-            <form name="contact" method="POST" data-netlify="true">
-                <p>
-                    <label>Your Name: <input type="text" name="name" /></label>
-                </p>
-                <p>
-                    <label>Your Email: <input type="email" name="email" /></label>
-                </p>
-                <p>
-                    <label>Your Role: <select name="role[]" multiple>
-                        <option value="leader">Leader</option>
-                        <option value="follower">Follower</option>
-                    </select></label>
-                </p>
-                <p>
-                    <label>Message: <textarea name="message"></textarea></label>
-                </p>
-                <p>
-                    <button type="submit">Send</button>
-                </p>
+                <button type='submit' className='btn btn-primary'>Send message</button>
             </form>
         </>
     )
