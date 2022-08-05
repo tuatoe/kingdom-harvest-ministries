77e91c586a81e98697a87149d4b9f9aaa7531ca8
Tuatoe Daynuah <tuaote.daynuah@gmail.com>
2022-08-05 13:37:01 -0500

    test form

--- pages/index.js
+++ pages/index.js
@@ -338,7 +338,27 @@ To further his education, he enrolled at Trinity Christian College where he grad
       <section className='section section-six'>
         <div className='container'>
           <h2 className='section-title'>Contact</h2>
-          <ContactFrom content='Please fill out this form and we will get in touch with you shortly.' />
+          <form name="contact" method="POST" data-netlify="true">
+            <p>
+              <label>Your Name: <input type="text" name="name" /></label>
+            </p>
+            <p>
+              <label>Your Email: <input type="email" name="email" /></label>
+            </p>
+            <p>
+              <label>Your Role: <select name="role[]" multiple>
+                <option value="leader">Leader</option>
+                <option value="follower">Follower</option>
+              </select></label>
+            </p>
+            <p>
+              <label>Message: <textarea name="message"></textarea></label>
+            </p>
+            <p>
+              <button type="submit">Send</button>
+            </p>
+          </form>
+          {/* <ContactFrom content='Please fill out this form and we will get in touch with you shortly.' /> */}
         </div>
       </section>
     </div>
