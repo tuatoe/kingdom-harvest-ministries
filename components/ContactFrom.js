512946ce33f560e85225ba98e369127e1d26d494
Tuatoe Daynuah <tuaote.daynuah@gmail.com>
2022-08-05 13:52:21 -0500

    cleanup

--- components/ContactFrom.js
+++ components/ContactFrom.js
@@ -4,20 +4,20 @@ function ContactFrom({ content }) {
     return (
         <>
             <p>{content}</p>
-            <form action='POST' data-netlify='true' name='bks-contact'>
-                <div className='fields'>
-                    <div className='field'>
-                        <input type='text' name='name' id='name' placeholder='Your name' required />
+            <form method="POST" data-netlify="true" name="bks-contact">
+                <div className="fields">
+                    <div className="field">
+                        <input type="text" name="name" id="name" placeholder="Your name" required />
                     </div>
-                    <div className='field'>
-                        <input type='text' name='email' id='email' placeholder='Your email' required />
+                    <div className="field">
+                        <input type="text" name="email" id="email" placeholder="Your email" required />
                     </div>
-                    <div className='field'>
-                        <textarea rows='7' name='message' id='message' placeholder='Your message' required></textarea>
+                    <div className="field">
+                        <textarea rows="7" name="message" id="message" placeholder="Your message" required></textarea>
                     </div>
                     <div netlify-recaptcha></div>
                 </div>
-                <button type='submit' className='btn btn-primary'>Send message</button>
+                <button type="submit" className="btn btn-primary">Send message</button>
             </form>
         </>
     )
