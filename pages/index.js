c55e10dac850f871ef23cb182460ae77778b1fe0
Tuatoe Daynuah <tuatoedaynuah@Tuatoes-MBP.hsd1.mn.comcast.net>
2023-01-22 18:30:00 -0600

    updates

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ const Home = ({ }) => {
           </p>
           <p>This mission includes empowering believers through teaching, preaching and living out the gospel in community and rekindling their passion to love God and serve him faithfully.</p>
 
-          <Link href='/about-us'>LEARN MORE</Link>
+          <Link href='/about-us'>LEARN MORE ABOUT US </Link>
         </div>
       </section>
 
@@ -28,7 +28,7 @@ const Home = ({ }) => {
         <div className='container'>
           <h2 className='section-title'>YOUTH MINISTRIES</h2>
           <p>Outreach to the youth is of major priority as we believe our youth have become the target of the circular world and anti-God movements. We believe God is raising up a generation of young men and women who would fight back and not accept the world’s philosophy, culture and ideas.</p>
-          <Link href='/what-we-do'>LEARN MORE</Link>
+          <Link href='/what-we-do'>LEARN MORE ABOUT YOUTH MINISTRIES</Link>
         </div>
       </section>
 
@@ -38,7 +38,7 @@ const Home = ({ }) => {
           <p>
             Women play a big role in our communities and face so many challenges. The women’s ministries partners with women to experience God at a new level of encounter, engagement, and commitment to do exploits for God and engage in the kingdom agenda on the home and community fronts and to involvement in local and global initiatives.
           </p>
-          <Link href='/how-you-can-help'>LEARN MORE</Link>
+          <Link href='/how-you-can-help'>LEARN MORE ABOUT WOMEN'S MINISTRIES</Link>
 
         </div>
       </section>
@@ -47,6 +47,7 @@ const Home = ({ }) => {
         <div className='container'>
           <h2 className='section-title'>GIVING</h2>
           <p>When you give to Kingdom Harvest Ministries your Donatins/gifts help us fulfill the mission to take the gospel to the ends of the earth. As a result of your generosity, we are able to train laborers in the harvest field, lives can be transformed, individuals, families, can live in their destinies, communities can experience the kingdom of God, the power of sin and wickedness can be destroyed in the lives of people.</p>
+          <Link href='/giving'>LEARN MORE ABOUT GIVING</Link>
         </div>
       </section>
 
