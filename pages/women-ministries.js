c65e699364281367f9078e82f8eaee0f6169bddd
Tuatoe Daynuah <tuatoedaynuah@Tuatoes-MBP.hsd1.mn.comcast.net>
2023-01-22 18:31:40 -0600

    cleanup

--- pages/women-ministries.js
+++ pages/women-ministries.js
@@ -14,15 +14,15 @@ const Giving = () => {
                 <h2 className='page-title'>Providing educational opportunities</h2>
                 <p>Women play a big role in our communities and face so many challenges. The
 
-women’s ministries partners with women to experience God at a new level of encounter, engagement,
+                    women’s ministries partners with women to experience God at a new level of encounter, engagement,
 
-and commitment to do exploits for God and engage in the kingdom agenda on the home and
+                    and commitment to do exploits for God and engage in the kingdom agenda on the home and
 
-community fronts and to involvement in local and global initiatives. Helping women discover their
+                    community fronts and to involvement in local and global initiatives. Helping women discover their
 
-purpose and destiny and walking in the abundant life provided in Jesus Christ is the core of the women’s
+                    purpose and destiny and walking in the abundant life provided in Jesus Christ is the core of the women’s
 
-ministries. Contact us to join our monthly Zoom meetings.</p>
+                    ministries. Contact us to join our monthly Zoom meetings.</p>
             </div>
         </>
     )
--- pages/youth-ministries.js
+++ pages/youth-ministries.js
@@ -13,15 +13,12 @@ const WomenMinistries = () => {
             <div className='container'>
                 <h2 className='page-title'>You Can Make A Difference</h2>
                 <p>Outreach to the youth is of major priority as we believe our youth have become the
-
-target of the circular world and anti-God movements. We believe God is raising up a generation of
-
-young men and women who would fight back and not accept the world’s philosophy, culture and ideas.
-
-We are committed to inspiring, empowering and equipping our youth to be set apart for God and be the
-
-catalyst of the move of God into the next generation of believers. Contact us to join our monthly Zoom
-meetings.</p>
+                    target of the circular world and anti-God movements. We believe God is raising up a generation of
+                    young men and women who would fight back and not accept the world’s philosophy, culture and ideas.
+                    We are committed to inspiring, empowering and equipping our youth to be set apart for God and be the
+                    catalyst of the move of God into the next generation of believers. Contact us to join our monthly Zoom
+                    meetings.
+                </p>
             </div>
         </>
     )
