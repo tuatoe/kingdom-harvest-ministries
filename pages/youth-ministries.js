4a38cab7675459b217dd72be6618d1f55613611c
Tuatoe Daynuah <tuatoedaynuah@Tuatoes-MBP.hsd1.mn.comcast.net>
2023-01-22 18:34:18 -0600

    fix special character

--- pages/youth-ministries.js
+++ pages/youth-ministries.js
@@ -14,7 +14,7 @@ const WomenMinistries = () => {
                 <h2 className='page-title'>You Can Make A Difference</h2>
                 <p>Outreach to the youth is of major priority as we believe our youth have become the
                     target of the circular world and anti-God movements. We believe God is raising up a generation of
-                    young men and women who would fight back and not accept the world’s philosophy, culture and ideas.
+                    young men and women who would fight back and not accept the world's philosophy, culture and ideas.
                     We are committed to inspiring, empowering and equipping our youth to be set apart for God and be the
                     catalyst of the move of God into the next generation of believers. Contact us to join our monthly Zoom
                     meetings.
